896d0cc56b1acd482ea9a600a066991e21d2efb7
tdharris <tylerdavidharris@gmail.com>
2026-07-21 15:01:45 -0600

    ci(test): fix macos

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,11 @@
 import { defineConfig } from "@vscode/test-cli";
+import os from "node:os";
+import path from "node:path";
 
 export default defineConfig({
   files: "out/test/**/*.test.js",
+  launchArgs: [
+    `--user-data-dir=${path.join(os.tmpdir(), "vsc-test-user-data")}`,
+  ],
 });
+
